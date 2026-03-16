9593829b6bc4f11115805cac1e1e624f66deb0d5
Dean-Cimatu <deancimatu@gmail.com>
2026-03-16 16:35:38 +0000

    implement Boot seq with dummy desktop

---
+++ src/components/Boot.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+export default function Boot() {
+  const [booting, setBooting] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setBooting(false);
+    }, 1500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <div>{booting ? "DeanOS Booting..." : "DeanOS Loaded!"}</div>;
+}
