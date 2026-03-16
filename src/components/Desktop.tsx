3ae89a5ee80ea10214fe190efb333b5f87082e66
Dean-Cimatu <deancimatu@gmail.com>
2026-03-16 23:03:54 +0000

    Add live clock to taskbar

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -1,25 +1,37 @@
+import {useState, useEffect} from "react";
+
 export default function Desktop() {
-  return <div>
-    <div style={{
-        width: '100vw',
-        height: '100vh',
-        backgroundColor:'black',
-        display: 'flex',
-        flexDirection: 'column',
-    }}>
-    <div style={{ flex : 1 }}>
-        </div>
-        <div style = {{
-            height : '40px',
-            backgroundColor: '#1a1a1a',
+
+    const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+    useEffect(()=> {
+       const interval =setInterval(() => {
+        setCurrentTime(new Date().toLocaleTimeString())
+    }, 1000)
+    return() => clearInterval(interval)
+},[])
+
+
+     return <div>
+     <div style={{
+            width: '100vw',
+            height: '100vh',
+            backgroundColor:'black',
             display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            padding: ' 0 12px',
+            flexDirection: 'column',
         }}>
-            <button>Start</button>
-            <p style={{ color: 'white'}}>22:00</p>
+        <div style={{ flex : 1 }}>
+            </div>
+            <div style = {{
+                height : '40px',
+                backgroundColor: '#1a1a1a',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                padding: ' 0 12px',
+            }}>
+                <button>Start</button>
+                <p style={{ color: 'white'}}>{currentTime}</p>
+                </div>
             </div>
         </div>
-    </div>
-}
+    }
