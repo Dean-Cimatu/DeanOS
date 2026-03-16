9409e8832fde2dc5e73d3312c7dac219fd964d3a
Dean-Cimatu <deancimatu@gmail.com>
2026-03-16 23:15:24 +0000

    Added Minor style to login screen

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,18 +3,36 @@ import { useState } from "react";
 export default function Login({ onComplete }: { onComplete: () => void }) {
   const [username, setUsername] = useState("");
 
-  return (
-    <div style={{ color: 'white' }}>
-      <p>Enter your username to log in</p>
-      <input
-        type="text"
-        value={username}
-        onChange={e => setUsername(e.target.value)}
-      />
-      <br />
-      <button onClick={() => onComplete()}>
-        Login
-      </button>
+      return(
+    <div style = {{
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+    }}>
+        <div style = {{
+            width: '45vw',
+            height: '300px',
+            backgroundColor: '#1a1a1a',
+            borderRadius: '8px',
+            padding: '24px',
+        }}>
+            <div style={{ color: 'white' }}>
+                <h2> Welcome to DeanOS! </h2>
+                <p>Enter your username to log in</p>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <br />
+                <button onClick={() => onComplete()}>
+                    Login
+                </button>
+            </div>
+        </div>
     </div>
-  );
+    )
 }
