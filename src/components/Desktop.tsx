fca3a10b1fbcf34920d133dc0930799057c454c2
Dean-Cimatu <deancimatu@gmail.com>
2026-03-16 22:45:13 +0000

    Added Desktop Shell with placeholder taskbar

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -1,3 +1,25 @@
 export default function Desktop() {
-  return <div>Desktop DeanOS Loaded</div>;
+  return <div>
+    <div style={{
+        width: '100vw',
+        height: '100vh',
+        backgroundColor:'black',
+        display: 'flex',
+        flexDirection: 'column',
+    }}>
+    <div style={{ flex : 1 }}>
+        </div>
+        <div style = {{
+            height : '40px',
+            backgroundColor: '#1a1a1a',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            padding: ' 0 12px',
+        }}>
+            <button>Start</button>
+            <p style={{ color: 'white'}}>22:00</p>
+            </div>
+        </div>
+    </div>
 }
