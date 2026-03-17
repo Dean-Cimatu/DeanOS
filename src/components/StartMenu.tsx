0f6a3a64aaa62f6e22e52fd161e23ed8175ddf78
Dean-Cimatu <deancimatu@gmail.com>
2026-03-17 15:38:14 +0000

    Bug fix for Start Menu

--- src/components/StartMenu.tsx
+++ src/components/StartMenu.tsx
@@ -1,3 +1,17 @@
 export default function StartMenu(){
-    return
+    return<div style ={{
+        width: '25vw',
+        height: '50vh',
+        alignContent: 'left',
+        justifyContent: 'left',
+        backgroundColor: '#1a1a1a',
+        position: 'absolute',
+        bottom: '40px',
+        left: '0',
+    }}>
+        <button style={{ display: 'block' }}>About Me!</button>
+        <button style={{ display: 'block' }}>Projects</button>
+        <button style={{ display: 'block' }}>Terminal</button>
+        <button style={{ display: 'block' }}>Contact</button>
+    </div>
 }
