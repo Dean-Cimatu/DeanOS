e5f3571eae0cd254eadb4d4bfc3abef9f5099ef3
Dean-Cimatu <deancimatu@gmail.com>
2026-03-18 11:20:49 +0000

    Added window render skeleton to Window.tsx

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -2,6 +2,27 @@ import { useWindowStore } from "../store/windowStore"
 import  StartMenu from "./StartMenu"
 
 export default function Window(){
-    const { closeWindow, focusWindow, moveWindow } = useWindowStore()
-    return    
+    const { closeWindow, focusWindow, moveWindow, minimiseWindow, openWindow } = useWindowStore()
+    return(<div>
+    <div style={{
+        width: "40vw",
+        height: "60vh",
+        backgroundColor: "white",
+        display: "flex",
+
+    }}
+    >
+    </div>
+    <div style={{
+        flex: 1,
+        width: "40vw",
+        height: "40px",
+        backgroundColor: "gray",
+        display: "flex",
+        alignContent: "top",
+        justifyContent: "top",
+    }}
+    >
+    </div>
+    </div>)
 }
