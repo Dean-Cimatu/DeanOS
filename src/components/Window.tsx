271b46c8c7f80f083798a92d9ae1e758a1c81c07
Dean-Cimatu <deancimatu@gmail.com>
2026-03-20 20:48:04 +0000

    Fixed window drag

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -2,6 +2,7 @@ import { useWindowStore } from "../store/windowStore"
 import type { WindowState } from "../store/windowStore"
 import { useRef } from 'react'
 import type { MouseEvent } from 'react'
+import { useEffect } from 'react'
 
 interface WindowProps{
     id: string
@@ -12,24 +13,36 @@ export default function Window({id, windowData}: WindowProps){
     const { closeWindow, focusWindow, moveWindow, minimiseWindow, openWindow } = useWindowStore()
 
     const isDragging = useRef<boolean>(false)
-    const dragStart= useRef<({x: number, y: number})>({x:0,y:0})
+    const dragStart= useRef<({mouseX: number, mouseY: number, winX: number, winY:number})>({mouseX:0,mouseY:0,winX:0, winY:0})
 
     const handleMouseDown =(e: React.MouseEvent) => {
+        
         isDragging.current = true
-        dragStart.current = {x:e.clientX, y: e.clientY}
+        dragStart.current = {mouseX:e.clientX, mouseY: e.clientY, winX: windowData.x, winY: windowData.y}
     }
     const handleMouseMove = (e: React.MouseEvent) => {
         if (!isDragging.current) return
 
-        const deltaX= e.clientX-dragStart.current.x
-        const deltaY= e.clientY-dragStart.current.y
+        const newX = dragStart.current.winX + (e.clientX - dragStart.current.mouseX)
+        const newY = dragStart.current.winY + (e.clientY - dragStart.current.mouseY)
 
-        moveWindow(id, windowData.x + deltaX, windowData.y + deltaY)
+        moveWindow(id, newX, newY)
+    
     }
     const handleMouseUp =() =>{
         isDragging.current = false
     }
     
+    useEffect(()=> {
+        const handleDocumentMouseUp = () => {
+            isDragging.current = false
+        }
+
+        document.addEventListener('mouseup', handleDocumentMouseUp)
+
+        return ()=> document.removeEventListener('mouseup', handleDocumentMouseUp)
+    },[])
+
     return(<div style={{
         position: 'absolute',
         left: windowData.x,
